c6093e86e629fedc5db0de32889d3bdb5809b0f3
Unknown <Valteziro@gmail.com>
2017-08-28 12:04:09 -0600

    Added Animal Scripts

---
+++ 1600Fall2017/Assets/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Die();
+	}
+
+	void Die () {
+		print(this.name + "Dies");
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
