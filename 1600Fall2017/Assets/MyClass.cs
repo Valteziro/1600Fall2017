26f15dad3fccad8f89c042fb9b62df373d2a7a16
Unknown <Valteziro@gmail.com>
2017-09-06 12:00:39 -0600

    added Float

--- 1600Fall2017/Assets/MyClass.cs
+++ 1600Fall2017/Assets/MyClass.cs
@@ -7,6 +7,7 @@ public class MyClass : MonoBehaviour {
     string myString = "Hello";
     string myWater = "Water";
     int myInt = 5;
+    float myFloat = 3.1
 
 	    // Use this for initialization
 	void Start ()
