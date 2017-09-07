5f95e0297b52501e2aee77e721eb4886a6b803c7
Unknown <Valteziro@gmail.com>
2017-09-06 18:55:41 -0600

    Conventions and Syntax

---
+++ 1600Fall2017/Assets/ConventionsAndSyntax.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConventionsAndSyntax : MonoBehaviour 
+
+	// This is an example of camelCasing. This IS a good convention to use in C#.
+	int myNumber1 = 1;
+	
+	// This is an example of PascalCasing. This IS a good convention to use in C#.
+	int MyNumber2 = 2;
+
+	// This is an example of Hungarian. This IS NOT a good convention to use in C#.
+	int iNumber3 = 3;
+
+{
+	void Start () 
+	{
+
+	// This is an example of GOOD syntax.
+	print("Hello World!");
+
+	// This is an example of BAD syntax.
+	print "Hello World!;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
