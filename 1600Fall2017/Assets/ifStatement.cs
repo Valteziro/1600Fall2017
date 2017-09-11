25178ff770ce9e2e18381cc2fb7f70acd2087d1f
Unknown <Valteziro@gmail.com>
2017-09-11 12:00:39 -0600

    Added IF statments

---
+++ 1600Fall2017/Assets/ifStatement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ifStatement : MonoBehaviour
+{
+
+    public int num1;
+    public int num2;
+    public int value;
+
+    public bool canPlay;
+
+    public Text input;
+    private string password = "G@m3Pl@y";
+    // Use this for initialization
+    void Update()
+    {
+        if (num1 + num2 == value)
+        {
+            print(value);
+        }
+
+        if (canPlay)
+        {
+            print("Play");
+        }
+
+        if (input.text == password)
+        {
+            print("You know the password");
+        }
+    }
+}
