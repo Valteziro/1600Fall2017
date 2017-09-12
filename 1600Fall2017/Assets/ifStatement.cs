ac9777a64a1059609325558b1ece5411d43852fc
Unknown <Valteziro@gmail.com>
2017-09-11 18:26:20 -0600

    Added 10 if statements

--- 1600Fall2017/Assets/ifStatement.cs
+++ 1600Fall2017/Assets/ifStatement.cs
@@ -7,18 +7,34 @@ public class ifStatement : MonoBehaviour
     public int num1;
     public int num2;
     public int value;
+    public int value2;
+    public int value3;
 
     public bool canPlay;
+    public bool False; 
 
     public Text input;
     private string password = "G@m3Pl@y";
-    // Use this for initialization
+
     void Update()
+    
     {
         if (num1 + num2 == value)
         {
             print(value);
         }
+
+        if (num1 * num2 == value2)
+        {
+            print(value2)
+        }
+
+        if (num1 % num2 == value3)
+        {
+            print(value3)
+        }
+
+        if 
 
         if (canPlay)
         {
@@ -28,6 +44,11 @@ public class ifStatement : MonoBehaviour
         if (input.text == password)
         {
             print("You know the password");
+        }
+
+        if (False)
+        {
+            print("It's False, therefore True")
         }
     }
 }
