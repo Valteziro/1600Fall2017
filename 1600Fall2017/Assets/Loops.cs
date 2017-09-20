54ab02cf5cf37c26afe9806a541236c1c87a97aa
Unknown <Valteziro@gmail.com>
2017-09-20 16:10:05 -0600

    Added WhileLoops with 10 Examples

--- 1600Fall2017/Assets/Loops.cs
+++ 1600Fall2017/Assets/Loops.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class Loops : MonoBehaviour {
     public string[] pickups;
 
-    public bool canPlay = true;
+    public bool CanPlay = true;
+    public bool CantPlay;
+    public bool WillPlay;
+    public bool WontPlay;
+    public bool WantToPlay;
+    public bool DontWantToPlay;
+    public bool WouldLikeToPlay;
+    public bool WouldntLikeToPlay;
+    public bool NeverPlay;
+    public bool AlwaysPlay;    
 
     void Start()
     {
@@ -14,9 +23,72 @@ public class Loops : MonoBehaviour {
 
     IEnumerator PlayGame ()
     {
-        while (canPlay)
+        while (CanPlay)
         {
             print("Playing");
+            yield return new WaitForSeconds(0.1f);
+        }
+        print("End Game");
+
+        while (CantPlay)
+        {
+            print("Can't Play");
+            yield return new WaitForSeconds(0.1f);
+        }
+        print("End Game");
+
+        while (WillPlay)
+        {
+            print("Will Play");
+            yield return new WaitForSeconds(0.1f);
+        }
+        print("End Game");
+
+        while (WontPlay)
+        {
+            print("Won't Play");
+            yield return new WaitForSeconds(0.1f);
+        }
+        print("End Game");
+
+        while (WantToPlay)
+        {
+            print("Want To Play");
+            yield return new WaitForSeconds(0.1f);
+        }
+        print("End Game");
+
+        while (DontWantToPlay)
+        {
+            print("Don't Want To Play");
+            yield return new WaitForSeconds(0.1f);
+        }
+        print("End Game");
+
+        while (WouldLikeToPlay)
+        {
+            print("Would Like To Play");
+            yield return new WaitForSeconds(0.1f);
+        }
+        print("End Game");
+
+        while (WouldntLikeToPlay)
+        {
+            print("Wouldn't Like To Play");
+            yield return new WaitForSeconds(0.1f);
+        }
+        print("End Game");
+
+        while (NeverPlay)
+        {
+            print("Never Play");
+            yield return new WaitForSeconds(0.1f);
+        }
+        print("End Game");
+
+        while (AlwaysPlay)
+        {
+            print("Always Play");
             yield return new WaitForSeconds(0.1f);
         }
         print("End Game");
--- 1600Fall2017/Assets/SwitchStatements.cs
+++ 1600Fall2017/Assets/SwitchStatements.cs
@@ -9,43 +9,43 @@ public class WeaponsSwitch : MonoBehaviour {
         switch (weapons[i])
         {
             case "Rhino":
-                print("Rhino");
+                print("Rhino equipped");
                 break;
 
             case "1911-A1":
-                print("1911-A1");
+                print("1911-A1 equipped");
                 break;
 
 			case "1860 Army":
-                print("1860 Army");
+                print("1860 Army equipped");
                 break;
 
 			case "Rough Rider .22":
-                print("Rough Rider .22");
+                print("Rough Rider .22 equipped");
                 break;	
 
 			case "870":
-                print("870");
+                print("870 equipped");
                 break;
 
 			case "1887":
-                print("1887");
+                print("1887 equipped");
                 break;
 
 			case "M91/30":
-                print("M91/30");
+                print("M91/30 equipped");
                 break;
 
 			case "C39V2":
-                print("C39V2");
+                print("C39V2 equipped");
                 break;
 
 			case "AA-15":
-                print("AA-15");
+                print("AA-15 equipped");
                 break;
 
 			case "CPX-2":
-                print("CPX-2");
+                print("CPX-2 equipped");
                 break;
 
             default:
