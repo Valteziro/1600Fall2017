2e28cae6b500c39e6bcf4c7f85d4d1f7f12cb3a3
Unknown <Valteziro@gmail.com>
2017-09-20 11:50:06 -0600

    Added whileLoops

---
+++ 1600Fall2017/Assets/Loops.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+    public string[] pickups;
+
+    public bool canPlay = true;
+
+    void Start()
+    {
+        StartCoroutine(PlayGame());
+    }
+
+    IEnumerator PlayGame ()
+    {
+        while (canPlay)
+        {
+            print("Playing");
+            yield return new WaitForSeconds(0.1f);
+        }
+        print("End Game");
+    }
+}
