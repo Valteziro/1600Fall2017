5e23294661d78e6d37bb167b9b5a206f7d4d258c
Unknown <Valteziro@gmail.com>
2017-10-04 17:35:16 -0600

    added ScopeAndAccess

--- 1600Fall2017/Assets/ScopeAndAccess.cs
+++ 1600Fall2017/Assets/ScopeAndAccess.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class ScopeAndAccess : MonoBehaviour
 {
-    //These can be called anywhere as they are in scope.
-    private int health = 100;
+    //These can be called anywhere as they are fields.
 
+    //These have the private access modifier, meaning they can only be called in this script.
+    private int health = 100;
+    private string MyString = "This is a string.";
+    private bool MyBool = true;
+    private float MyFloat = 2.5f;
+    private double MyDouble = 2.5;
+    
+    //These have the public access modifier, meaning they can be called in other scripts.
     public int score = 100;
+    public string MyString2 = "This is also a string.";
+    public bool MyBool2 = true;
+    public float MyFloat2 = 2.5f;
+    public double MyDouble2 = 2.5;
 
     private void Start()
     {
