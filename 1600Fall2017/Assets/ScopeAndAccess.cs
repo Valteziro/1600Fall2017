66b3aaab7c0464abe06cd86709423268fca69625
Unknown <Valteziro@gmail.com>
2017-10-04 12:13:13 -0600

    added ScopeAndAccess

---
+++ 1600Fall2017/Assets/ScopeAndAccess.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeAndAccess : MonoBehaviour
+{
+    //These can be called anywhere as they are in scope.
+    private int health = 100;
+
+    public int score = 100;
+
+    private void Start()
+    {
+        //These cannot be called anywhere as they are in brackets.
+        int NewScore = 10;
+        print(NewScore);
+    }
+
+
+}
