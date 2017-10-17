a78e5688439851337bd5bad8f7428cea1d1652ea
Unknown <Valteziro@gmail.com>
2017-10-17 17:04:43 -0600

    added Complete Roll A Ball Game

--- Roll A Ball/Assets/Scripts/PlayerController.cs
+++ Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour 
 {
 	public float Speed;
+	public Text countText;
+	public Text winText;
 	private Rigidbody rb;
+	private int count;
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 	void FixedUpdate ()
 	{
 		float MoveHorizontal = Input.GetAxis ("Horizontal");
-		float MoveVertical = Input.GetAxis ("Vertical");
-		
-		Vector3 Movement = new Vector3 (MoveHorizontal, 0.0f, MoveVertical);
-		
+		float MoveVertical = Input.GetAxis ("Vertical");		
+		Vector3 Movement = new Vector3 (MoveHorizontal, 0.0f, MoveVertical);		
 		rb.AddForce (Movement * Speed);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pickup"))
+		{
+			other.gameObject.SetActive (false);
+			count = ++count;
+			SetCountText ();
+		}
+	}
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12)
+		{
+			winText.text = "You Win!";
+		}
 	}
 }
--- Roll A Ball/Assets/Scripts/Rotator.cs
+++ Roll A Ball/Assets/Scripts/Rotator.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour {
-
-	// Update is called once per frame
+public class Rotator : MonoBehaviour 
+{
 	void Update () 
 	{
 		transform.Rotate (new Vector3 (15, 30, 45)* Time.deltaTime);
