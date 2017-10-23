0e3377a4dc9607b9c6b2498d898a34d1ea4b8992
Unknown <Valteziro@gmail.com>
2017-10-23 12:05:27 -0600

    update 10/23/17

---
+++ 1600Fall2017/Assets/Scripts/UI/UIHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHealth : MonoBehaviour
+{
+    public Image bar;
+    public float barTime = 0.1f;
+
+	void OnTriggerEnter ()
+    {
+        StartCoroutine(UpdateBar());
+	}
+
+    IEnumerator UpdateBar()
+    {
+        while (bar.fillAmount < 1)
+        {
+            bar.fillAmount -= barTime;
+            yield return new WaitForSeconds(barTime);
+        }
+    }
+}
