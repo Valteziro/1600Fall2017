af558e865b455f9cd5fd0bbdefe4c843358cbbf3
Unknown <Valteziro@gmail.com>
2017-10-25 12:04:17 -0600

    update 10/25/17

--- 1600Fall2017/Assets/Scripts/UI/UIHealth.cs
+++ 1600Fall2017/Assets/Scripts/UI/UIHealth.cs
@@ -7,17 +7,48 @@ public class UIHealth : MonoBehaviour
 {
     public Image bar;
     public float barTime = 0.1f;
+    public float PowerLevel = 0.1f;
+    public float IncrementUp = 0.01f;
 
-	void OnTriggerEnter ()
+    public enum PowerUpType
     {
-        StartCoroutine(UpdateBar());
+        PowerUp,
+        PowerDown
+    }
+    public PowerUpType PowerUp;
+
+    void OnTriggerEnter ()
+    {
+        switch (PowerUp)
+        {
+            case PowerUpType.PowerUp:
+                StartCoroutine(PowerUpBar());
+            break;
+
+            case PowerUpType.PowerDown:
+                StartCoroutine(PowerDownBar());
+                break;
+        }
 	}
 
-    IEnumerator UpdateBar()
+    IEnumerator PowerUpBar()
     {
-        while (bar.fillAmount < 1)
+        while (PowerLevel < 1)
         {
-            bar.fillAmount -= barTime;
+            bar.fillAmount += IncrementUp;
+            yield return new WaitForSeconds(barTime);
+        }
+    }
+
+    IEnumerator PowerDownBar()
+    {
+        float TempAmount = PowerLevel;
+        float FillAmount = bar.fillAmount;
+        while (PowerLevel > 0)
+        {
+            TempAmount -= IncrementUp;
+            //FillAmount = TempAmount - IncrementUp;
+            bar.fillAmount -= TempAmount;
             yield return new WaitForSeconds(barTime);
         }
     }
