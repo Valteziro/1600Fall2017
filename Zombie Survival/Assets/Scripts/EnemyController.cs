4853d46a6bdd8f6f2ec56ded6c5d82dc6619a878
Unknown <Valteziro@gmail.com>
2017-11-12 18:56:25 -0700

    Final Project Update #5
    Added auto player tracking

---
+++ Zombie Survival/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour 
+{
+    public NavMeshAgent agent;
+    public Transform player;
+
+    void Awake ()
+    {
+        player = GameObject.Find ("Player").transform;
+        agent = GetComponent <NavMeshAgent> ();
+    }
+	void Update ()
+	{
+        agent.destination = player.position;
+	}
+}
