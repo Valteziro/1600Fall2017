6a4e6a689f5bd2aa9ddb60f300e8d33c6652b948
Unknown <Valteziro@gmail.com>
2017-11-27 20:58:44 -0700

    Final Project Update #8
    Added comments to the scripts

--- Zombie Survival/Assets/Scripts/PlayerController.cs
+++ Zombie Survival/Assets/Scripts/PlayerController.cs
@@ -13,17 +13,22 @@ public class PlayerController : MonoBehaviour {
     private Vector3 movement;
 	void Update()
     {
+        //checks for gameOver, if gameOver is not true, the player can be controlled. If gameOver is true, the player cannot be controlled.
         if (!gameOver)
         {
+            //assigns x and z movement to key tagged "Horizontal" and "Vertical" within Unity.
             movement.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
             movement.z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         }
+        //attaches this script to the characterController within Unity.
         characterController.Move(movement);
     }
     void OnTriggerEnter (Collider other) 
 	{
+        //searches for contact with gameObjects tagged "Healthkit"
 		if(other.gameObject.CompareTag("Healthkit")) 
 		{
+            //Deactivates "Healthkit" on contact.
 			other.gameObject.SetActive(false);
 		}
 	}
--- Zombie Survival/Assets/Scripts/UIScript.cs
+++ Zombie Survival/Assets/Scripts/UIScript.cs
@@ -10,7 +10,8 @@ public class UIScript : MonoBehaviour
     public GameObject gameOverUI;
     public float powerLevel = 0.1f;
     public float amountToAdd = 0.01f;   
-	public PowerUpType DamageOrHeal; 
+	public PowerUpType DamageOrHeal;
+    //Lists all "PowerUpTypes"
     public enum PowerUpType	
     {
         Healthkit,
@@ -18,12 +19,15 @@ public class UIScript : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
+        //lists both "Healthkit" and "EnemyAttack" under a drop down menu in Unity's inspector.
         switch (DamageOrHeal)
         {
+            //lists type of "DamageOrHeal" "Healthkit"
             case PowerUpType.Healthkit:
                 StartCoroutine(HealthKitBar());
                 break;
 
+            //lists type of "DamageOrHeal" "EnemyAttack"
             case PowerUpType.EnemyAttack:
                 StartCoroutine(EnemyAttackBar());
                 break;
@@ -31,35 +35,50 @@ public class UIScript : MonoBehaviour
     }
     IEnumerator HealthKitBar()
     {
+        //makes type "HealthKitBar" add health from the health bar.
         float tempAmount = bar.fillAmount + powerLevel;
+        //checks to see if the health bar is over 100%
         if (tempAmount > 1)
         {
+            //sets the health bar to 100% if the statement comes back true, stops the health bar from going over 100%.
             tempAmount = 1;
         }
 
+        //checks to see if the amount added is greater than the bar fill amount.
         while (bar.fillAmount < tempAmount)
         {
+            //adds to the health bar when the while statement is true.
             bar.fillAmount += amountToAdd;
+            //adds a debounce to the amount to add.
             yield return new WaitForSeconds(amountToAdd);
         }
     }
     IEnumerator EnemyAttackBar()
     {
+        //makes type "EnemyAttackBar" subtract health from the health bar.
         float tempAmount = bar.fillAmount - powerLevel;
+        //checks to see if the health bar is over 0%
         if (tempAmount < 0)
         {
+            //sets the health bar to 0% if the statement comes back true, stops the health bar from going under 0%.
             tempAmount = 0;
         }
 
+        //checks to see if the amount added is less than the bar fill amount.
         while (bar.fillAmount > tempAmount)
         {
+            //subtracts from the health bar when the while statement is true.
             bar.fillAmount -= amountToAdd;
+            //adds a debounce to the amount to subtract.
             yield return new WaitForSeconds(amountToAdd);
         }
-
+        
+        //checks to see if the health bar is at 0%
         if (bar.fillAmount == 0)
         {
+            //sets the gameOverUI to active if the health bar reaches 0%
             gameOverUI.SetActive(true);
+            //when gameOver is true, sends PlayerController a gameOver = true message.
             PlayerController.gameOver = true;
         }
     }
