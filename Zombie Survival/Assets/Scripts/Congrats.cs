0fd8cf654312ff986bb4ee817b9977d6c37879e9
Unknown <Valteziro@gmail.com>
2017-12-04 11:21:58 -0700

    Final Project #13
    added gj text

--- Zombie Survival/Assets/Scripts/Congrats.cs
+++ Zombie Survival/Assets/Scripts/Congrats.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Congrats : MonoBehaviour
 {
     public Text goodJob;
 	
-	void Update ()
+	void OnTriggerEnter ()
     {
         string[] Grats = { "You can do it!","Good job!","Keep going!" };
         foreach (string gj in Grats)
         {
-            goodJob.text = ("{0} ", gj);
+            goodJob.text = (gj);
         }
 	}
 }
